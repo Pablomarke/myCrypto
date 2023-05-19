0cf21b35c2c09fd3fb8a3b86b72e5df96821e723
Pablomarke <pablomarquezartesano@gmail.com>
2023-05-19 17:52:55 +0200

    Solucionado errores en vistas e implementada correctamente la barrra de anvegación

--- Desktop/my_Crypto/my_Crypto/routes.py
+++ Desktop/my_Crypto/my_Crypto/routes.py
@@ -1,18 +1,22 @@
 from flask import render_template
 from my_Crypto import app
 from my_Crypto.modelos import select_all
-
+import requests
+from key import APIKEY
 
 @app.route('/')
 def index():
     tabla = select_all()
 
-    return render_template("index.html", data = tabla)
+    return render_template("index.html", data = tabla, title = "Inicio")
 
 @app.route("/purchase")
+
+
+
 def compra():
-    return render_template("compra.html")
+    return render_template("compra.html", title = "Compra", page = "Compra")
 
 @app.route("/status")
 def estado():
-    return render_template("estado.html")
+    return render_template("estado.html", title = "Estado", page = "Estado")
